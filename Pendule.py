d3a6510224882098ea69c3bb791b44062bded320
ilredonald <donaldtchoupii@gmail.com>
2024-12-07 19:28:35 +0100

    update

--- Pendule.py
+++ Pendule.py
@@ -12,7 +12,9 @@ donnees = {
     "points" : 10
 }
 
+print(f'-:-:{donnees["mot_deviner"]} le mot à deviner a ({len(donnees["mot_deviner"])} lettres) -:-:- vous avez un score de: {donnees["points"]} points')
 while True:
+   
     lettre = input("--renseignez une lettre: ")
     
     if lettre in donnees["mot_secret"] and lettre not in donnees["mot_deviner"]:
@@ -23,13 +25,16 @@ while True:
         donnees["mot_deviner"] = ''.join(liste)
     elif lettre not in  donnees["mot_secret"]:
         donnees["points"] -=1
-        
-    print(f'-:-:{donnees["mot_deviner"]} -:-:- score: {donnees["points"]}')
+    count = 0
+    for i in   donnees["mot_deviner"]:
+        if i == '_':
+            count += 1  
+    print(f'-:-:{donnees["mot_deviner"]} -:-:- votre score est de: {donnees["points"]} point(s) et il ne vous reste que {count} lettre(s) à deviner')
     
     if '_' not in donnees["mot_deviner"]:
-        print(f"-:-:- Félicitaions, vous avez gagné la partie avec un score de {donnees["points"]} !!! -:-:-")
+        print(f"-:-:- Félicitaions, vous avez gagné la partie avec un score de {donnees["points"]} point(s) !!! -:-:-")
         break
     
     if donnees["points"] == 0:
-        print('__Malheuresement vous avez perdu la partie.__La prochaine fois ça sera la bonne mola!')
+        print('__Malheuresement vous avez perdu la partie.__La prochaine fois ça sera la bonne, mola!')
         break
