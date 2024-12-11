22faf4bb8774775f461b6a03b74ad5361b0565e1
ilredonald <donaldtchoupii@gmail.com>
2024-12-11 21:24:33 +0100

    optimisation de l'affichage

--- Pendule.py
+++ Pendule.py
@@ -1,6 +1,6 @@
 
 import random
-mots = "stupide authentique festival content laver le cerveau gymnastique"
+mots = "language programmation vlan routeur switch  address machine learning"
 
 mots = mots.split()
 
@@ -9,16 +9,18 @@ index_secret = random.randint(0, len(mots) - 1 )
 donnees = {
     "mot_secret" : mots[index_secret],
     "mot_deviner" : "_" * len(mots[index_secret]),
-    "points" : 10
-}
+    "points" : 10,
+    "historique": set()
+        }
 
 print(f'-:-:{donnees["mot_deviner"]} le mot à deviner a ({len(donnees["mot_deviner"])} lettres) -:-:- vous avez un score de: {donnees["points"]} points')
 while True:
    
     lettre = input("--renseignez une lettre: ")
-    
+    donnees["historique"].add(lettre)
     if lettre in donnees["mot_secret"] and lettre not in donnees["mot_deviner"]:
         liste = list(donnees["mot_deviner"])
+       
         for i, lettre_secret in enumerate(donnees["mot_secret"]):
             if lettre == lettre_secret:
                 liste[i] = lettre
@@ -29,7 +31,7 @@ while True:
     for i in   donnees["mot_deviner"]:
         if i == '_':
             count += 1  
-    print(f'-:-:{donnees["mot_deviner"]} -:-:- votre score est de: {donnees["points"]} point(s) et il ne vous reste que {count} lettre(s) à deviner')
+    print(f'''-:-:{donnees["mot_deviner"]} -:-:- score : {donnees["points"]} point(s). {count} lettre(s) restantes:. lettre(s) renseignées {donnees["historique"]}''')
     
     if '_' not in donnees["mot_deviner"]:
         print(f"-:-:- Félicitaions, vous avez gagné la partie avec un score de {donnees["points"]} point(s) !!! -:-:-")
